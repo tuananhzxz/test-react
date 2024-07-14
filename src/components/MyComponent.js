3e2a2473ae33a5bc680141eb467a523731354bc4
TuanAnh <tuananhleonat@gmail.com>
2024-07-14 12:49:55 +0700

    init

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
+
+export default MyComponent
